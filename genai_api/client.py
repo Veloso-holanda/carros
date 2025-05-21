6df47625397372616cb0aa7a8553c4d007f8e770
Gabriel Veloso De Souza <holanda.gvs@gmail.com>
2025-05-20 22:42:28 -0400

    Guardando a chave da api

--- genai_api/client.py
+++ genai_api/client.py
@@ -1,11 +1,14 @@
 from google import genai
+import os
+from dotenv import load_dotenv
+load_dotenv()
 
 
 def get_car_ai_bio(model, brand, year):
     prompt = '''
     Me mostre uma descrição de venda para o carro {} {} {} em apenas 250 caracteres. Fale coisas específicas desse modelo
     '''
-    client = genai.Client(api_key="AIzaSyCzeJmW43jSN-HFZ6zenYb8FE7qbXKQ-n8")
+    client = genai.Client(api_key=os.getenv("GEMINI_API_KEY"))
     prompt = prompt.format(brand, model, year)
     response = client.models.generate_content(
         model="gemini-2.0-flash", 
